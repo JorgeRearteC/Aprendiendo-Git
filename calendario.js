5359f0ec3001ca3c4cb38c0bb5bad599984d9c48
JorgeRRC <jorgereartec@gmail.com>
2018-02-24 11:46:51 -0300

    Se finalizo de realizar cambios

--- calendario.js
+++ calendario.js
@@ -1,6 +1,4 @@
-CSS  JS  Result
-EDIT ON
- var mes_text = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+var mes_text = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
 var dia_text = ["Dom", "Lun", "Mar", "Mie", "Juv", "Vie", "Sab"];
 
